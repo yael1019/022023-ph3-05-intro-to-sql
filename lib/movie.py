dd8eaf3492e0c59ac2ed69e1dd6d4a57447a0be1
Yael Harosh <yaelharosh01@gmail.com>
2023-04-07 12:07:13 -0400

    notes/practice

--- lib/movie.py
+++ lib/movie.py
@@ -2,10 +2,23 @@ from . import CONN
 from . import CURSOR
 
 class Movie:
-    def __init__(self, title, runtime):
+    def __init__(self, title, year):
         self.id = None
         self.title = title
-        self.runtime = runtime
+        self.year = year
 
     def __repr__(self):
         return f'<Movie id={self.id} title={self.title} runtime={self.runtime}>'
+
+# object relational mapping / ORM 
+# everytime i have a table in my db, there is a class i use in python to match up with that table
+    @classmethod
+    def create_table(cls):
+        sql = """ CREATE TABLE IF NOT EXISTS movies (
+            id INTEGER PRIMARY KEY,
+            title TEXT,
+            year INTEGER
+        )
+        """
+        CURSOR.execute(sql)
+
